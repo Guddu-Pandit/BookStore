f16bbfe07faaff809d9c7fce980e572fb272d346
Guddu Kr. Pandit <guddukrp769@gmail.com>
2025-12-02 18:02:33 +0530

    connecting db

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,19 @@ const app = express()
 
 dotenv.config()
 
-const PORT= process.env.PORT || 4000
+const PORT= process.env.PORT || 4000;
+const URL=process.env.MONGO_URL;
 
-app.get('/', (req, res) => {
-  res.send('Hello guddu Kumar Pandit')
-})
+
+try {
+  mongoose.connect(URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+  });
+  console.log("Connected to MongoDB");
+}catch (error) {
+  console.log("Error: ",error);
+}
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
